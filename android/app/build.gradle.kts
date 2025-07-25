2d94d01dfedb0b42625725532e72ea8eba36c869
Harsh Patel <hp97808@gmail.com>
2025-07-25 22:11:47 +0530

    set ndkVersion

--- android/app/build.gradle.kts
+++ android/app/build.gradle.kts
@@ -8,7 +8,7 @@ plugins {
 android {
     namespace = "com.harsh.instagram"
     compileSdk = flutter.compileSdkVersion
-    ndkVersion = flutter.ndkVersion
+    ndkVersion = "27.0.12077973"
 
     compileOptions {
         sourceCompatibility = JavaVersion.VERSION_11
