762a7f1fdb3e0a0b2fe3c74fca53f1948db2cfc2
Harsh Patel <hp97808@gmail.com>
2025-08-03 21:51:16 +0530

    add nav bar and screens

--- lib/screens/home_screen.dart
+++ lib/screens/home_screen.dart
@@ -1,6 +1,8 @@
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
 import 'package:instagram/providers/user_provider.dart';
+import 'package:instagram/theme/theme.dart';
+import 'package:instagram/utils/global_variables.dart';
 import 'package:provider/provider.dart';
 import 'package:instagram/models/user.dart' as model;
 
@@ -15,30 +17,37 @@ class HomeScreen extends StatefulWidget {
 
 class _HomeScreenState extends State<HomeScreen> {
   String username = "";
+  int _page = 0;
+  late PageController pageController;
 
   @override
   void initState() {
     super.initState();
 
+    pageController = PageController();
     addData();
-    // getUserName();
+  }
+
+  @override
+  void dispose() {
+    super.dispose();
+    pageController.dispose();
+  }
+
+  void navigationTapped(int page) {
+    pageController.jumpToPage(page);
+  }
+
+  void onPageChanged(int page) {
+    setState(() {
+      _page = page;
+    });
   }
 
   Future<void> addData() async {
     UserProvider userProvider = Provider.of(context, listen: false);
     await userProvider.refreshUser();
   }
-
-  // void getUserName() async {
-  //   DocumentSnapshot snapshot = await FirebaseFirestore.instance
-  //       .collection('users')
-  //       .doc(FirebaseAuth.instance.currentUser!.uid)
-  //       .get();
-  //
-  //   setState(() {
-  //     username = snapshot.get('username');
-  //   });
-  // }
 
   void logOut() {
     FirebaseAuth.instance.signOut();
@@ -53,55 +62,73 @@ class _HomeScreenState extends State<HomeScreen> {
     model.User? user = Provider.of<UserProvider>(context).getUser;
 
     return Scaffold(
+      body: PageView(
+        controller: pageController,
+        onPageChanged: onPageChanged,
+        // physics: ,
+        children: homeScreenItems,
+      ),
+      // body: Center(
+      //   child: Column(
+      //     mainAxisAlignment: MainAxisAlignment.center,
+      //     children: [
+      //       Text(user != null ? user.username : 'Loading...'),
+      //       GestureDetector(
+      //         onTap: logOut,
+      //         child: Container(
+      //           padding: EdgeInsets.symmetric(vertical: 8),
+      //           child: Text('Log out'),
+      //         ),
+      //       ),
+      //     ],
+      //   ),
+      // ),
       bottomNavigationBar: BottomNavigationBar(
         type: BottomNavigationBarType.fixed,
+        currentIndex: _page,
+        onTap: navigationTapped,
         items: [
           BottomNavigationBarItem(
-            icon: Icon(Icons.home),
-            label: '',
+            icon: Icon(
+              Icons.home,
+              color: _page == 0 ? AppColors.primary : AppColors.secondary,
+            ),
+            label: 'Home',
             // backgroundColor: primaryColor,
           ),
           BottomNavigationBarItem(
-            icon: Icon(Icons.search),
-            label: '',
+            icon: Icon(
+              Icons.search,
+              color: _page == 1 ? AppColors.primary : AppColors.secondary,
+            ),
+            label: 'Search',
             // backgroundColor: primaryColor,
           ),
           BottomNavigationBarItem(
-            icon: Icon(Icons.add_circle_rounded),
-            label: '',
+            icon: Icon(
+              Icons.add_circle_rounded,
+              color: _page == 2 ? AppColors.primary : AppColors.secondary,
+            ),
+            label: 'Add',
             // backgroundColor: primaryColor,
           ),
           BottomNavigationBarItem(
-            icon: Icon(Icons.favorite_outlined),
-            label: '',
+            icon: Icon(
+              Icons.favorite_outlined,
+              color: _page == 3 ? AppColors.primary : AppColors.secondary,
+            ),
+            label: 'Fav',
             // backgroundColor: primaryColor,
           ),
           BottomNavigationBarItem(
-            icon: Icon(Icons.search),
-            label: '',
-            // backgroundColor: primaryColor,
-          ),
-          BottomNavigationBarItem(
-            icon: Icon(Icons.person),
-            label: '',
+            icon: Icon(
+              Icons.person,
+              color: _page == 4 ? AppColors.primary : AppColors.secondary,
+            ),
+            label: 'Pro',
             // backgroundColor: primaryColor,
           ),
         ],
-      ),
-      body: Center(
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: [
-            Text(user != null ? user.username : 'Loading...'),
-            GestureDetector(
-              onTap: logOut,
-              child: Container(
-                padding: EdgeInsets.symmetric(vertical: 8),
-                child: Text('Log out'),
-              ),
-            ),
-          ],
-        ),
       ),
     );
   }
