e6ca5beef02bd5f10fb7d98111db3d47b0be7140
Harsh Patel <hp97808@gmail.com>
2025-08-03 21:51:37 +0530

    add global variables

---
+++ lib/utils/global_variables.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/material.dart';
+import 'package:instagram/screens/add_post_screen.dart';
+
+const homeScreenItems = [
+  Text('feed'),
+  Text('search'),
+  AddPostScreen(),
+  Text('notif'),
+  Text('profile'),
+];
