d8e44a3611d078f684875759dd5fc7fa5151000c
Harsh Patel <hp97808@gmail.com>
2025-08-05 11:50:44 +0530

    change profile screen

--- lib/utils/global_variables.dart
+++ lib/utils/global_variables.dart
@@ -1,10 +1,11 @@
 import 'package:flutter/material.dart';
 import 'package:instagram/screens/add_post_screen.dart';
+import 'package:instagram/screens/profile_screen.dart';
 
 const homeScreenItems = [
   Text('feed'),
   Text('search'),
   AddPostScreen(),
   Text('notif'),
-  Text('profile'),
+  ProfileScreen(),
 ];
