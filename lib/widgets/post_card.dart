dbd21be2774cb290e022fcc538921638d6a659a4
Harsh Patel <hp97808@gmail.com>
2025-08-06 18:56:03 +0530

    add image

--- lib/widgets/post_card.dart
+++ lib/widgets/post_card.dart
@@ -70,6 +70,14 @@ class PostCard extends StatelessWidget {
               ],
             ),
           ),
+          SizedBox(
+            height: MediaQuery.of(context).size.height * 0.35,
+            width: double.infinity,
+            child: Image.network(
+              'https://plus.unsplash.com/premium_photo-1664474619075-644dd191935f?fm=jpg&q=60&w=3000&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxzZWFyY2h8MXx8aW1hZ2V8ZW58MHx8MHx8fDA%3D',
+              fit: BoxFit.cover,
+            ),
+          ),
         ],
       ),
     );
