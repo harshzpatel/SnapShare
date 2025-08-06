f2196b21d6c0f9d1c4eeaa08f3ebdb6fabc85baf
Harsh Patel <hp97808@gmail.com>
2025-08-06 21:47:09 +0530

    extract header

--- lib/widgets/post_card.dart
+++ lib/widgets/post_card.dart
@@ -11,65 +11,7 @@ class PostCard extends StatelessWidget {
       padding: EdgeInsets.symmetric(vertical: 10),
       child: Column(
         children: [
-          Container(
-            padding: EdgeInsets.symmetric(
-              vertical: 4,
-              horizontal: 16,
-            ).copyWith(right: 0),
-            child: Row(
-              children: [
-                CircleAvatar(
-                  radius: 16,
-                  backgroundImage: NetworkImage(
-                    'https://yt3.ggpht.com/yti/ANjgQV8sJ3Ji-ggJxkWTzwW6qwsSQQiARYU9gobaM2O6HUflT6hB=s88-c-k-c0x00ffffff-no-rj',
-                  ),
-                ),
-                Expanded(
-                  child: Padding(
-                    padding: EdgeInsets.only(left: 8),
-                    child: Column(
-                      mainAxisSize: MainAxisSize.min,
-                      crossAxisAlignment: CrossAxisAlignment.start,
-                      children: [
-                        Text(
-                          'username',
-                          style: TextStyle(fontWeight: FontWeight.bold),
-                        ),
-                      ],
-                    ),
-                  ),
-                ),
-                IconButton(
-                  onPressed: () {
-                    showDialog(
-                      context: context,
-                      builder: (context) => Dialog(
-                        child: ListView(
-                          padding: EdgeInsets.symmetric(vertical: 16),
-                          shrinkWrap: true,
-                          children: ['Delete']
-                              .map(
-                                (e) => InkWell(
-                                  onTap: null,
-                                  child: Container(
-                                    padding: EdgeInsets.symmetric(
-                                      vertical: 12,
-                                      horizontal: 16,
-                                    ),
-                                    child: Text(e),
-                                  ),
-                                ),
-                              )
-                              .toList(),
-                        ),
-                      ),
-                    );
-                  },
-                  icon: Icon(Icons.more_vert, color: AppColors.primary),
-                ),
-              ],
-            ),
-          ),
+          _header(context),
           SizedBox(
             height: MediaQuery.of(context).size.height * 0.35,
             width: double.infinity,
@@ -106,5 +48,67 @@ class PostCard extends StatelessWidget {
         ],
       ),
     );
+  }
+
+  Container _header(BuildContext context) {
+    return Container(
+          padding: EdgeInsets.symmetric(
+            vertical: 4,
+            horizontal: 16,
+          ).copyWith(right: 0),
+          child: Row(
+            children: [
+              CircleAvatar(
+                radius: 16,
+                backgroundImage: NetworkImage(
+                  'https://yt3.ggpht.com/yti/ANjgQV8sJ3Ji-ggJxkWTzwW6qwsSQQiARYU9gobaM2O6HUflT6hB=s88-c-k-c0x00ffffff-no-rj',
+                ),
+              ),
+              Expanded(
+                child: Padding(
+                  padding: EdgeInsets.only(left: 8),
+                  child: Column(
+                    mainAxisSize: MainAxisSize.min,
+                    crossAxisAlignment: CrossAxisAlignment.start,
+                    children: [
+                      Text(
+                        'username',
+                        style: TextStyle(fontWeight: FontWeight.bold),
+                      ),
+                    ],
+                  ),
+                ),
+              ),
+              IconButton(
+                onPressed: () {
+                  showDialog(
+                    context: context,
+                    builder: (context) => Dialog(
+                      child: ListView(
+                        padding: EdgeInsets.symmetric(vertical: 16),
+                        shrinkWrap: true,
+                        children: ['Delete']
+                            .map(
+                              (e) => InkWell(
+                                onTap: null,
+                                child: Container(
+                                  padding: EdgeInsets.symmetric(
+                                    vertical: 12,
+                                    horizontal: 16,
+                                  ),
+                                  child: Text(e),
+                                ),
+                              ),
+                            )
+                            .toList(),
+                      ),
+                    ),
+                  );
+                },
+                icon: Icon(Icons.more_vert, color: AppColors.primary),
+              ),
+            ],
+          ),
+        );
   }
 }
