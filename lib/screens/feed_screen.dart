5744da32c6576b5e58608164b3b6d02f21cf5396
Harsh Patel <hp97808@gmail.com>
2025-08-09 21:20:13 +0530

    add stream builder

--- lib/screens/feed_screen.dart
+++ lib/screens/feed_screen.dart
@@ -1,7 +1,6 @@
 import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_svg/flutter_svg.dart';
-import 'package:instagram/utils/utils.dart';
 import 'package:instagram/widgets/post_card.dart';
 
 import '../theme/theme.dart';
