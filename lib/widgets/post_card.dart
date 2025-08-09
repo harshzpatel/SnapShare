d84cb2c9db9cd3b936f4e7acbeffd630098d1f04
Harsh Patel <hp97808@gmail.com>
2025-08-09 19:46:10 +0530

    add description

--- lib/widgets/post_card.dart
+++ lib/widgets/post_card.dart
@@ -9,7 +9,87 @@ class PostCard extends StatelessWidget {
     return Container(
       color: AppColors.background,
       padding: EdgeInsets.symmetric(vertical: 10),
-      child: Column(children: [_header(context), _image(context), _buttons()]),
+      child: Column(
+        children: [
+          _header(context),
+          _image(context),
+          _buttons(),
+          Container(
+            padding: const EdgeInsets.symmetric(horizontal: 16),
+            child: Column(
+              mainAxisSize: MainAxisSize.min,
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: <Widget>[
+                DefaultTextStyle(
+                    style: Theme.of(context)
+                        .textTheme
+                        .titleSmall!
+                        .copyWith(fontWeight: FontWeight.w800),
+                    child: Text(
+                      '1000 likes',
+                      // '${widget.snap['likes'].length} likes',
+                      style: Theme.of(context).textTheme.bodyMedium,
+                    )),
+                Container(
+                  width: double.infinity,
+                  padding: const EdgeInsets.only(
+                    top: 8,
+                  ),
+                  child: RichText(
+                    text: TextSpan(
+                      style: const TextStyle(color: AppColors.primary),
+                      children: [
+                        TextSpan(
+                          text: 'username',
+                          // text: widget.snap['username'].toString(),
+                          style: const TextStyle(
+                            fontWeight: FontWeight.bold,
+                          ),
+                        ),
+                        TextSpan(
+                          text: ' description',
+                          // text: ' ${widget.snap['description']}',
+                        ),
+                      ],
+                    ),
+                  ),
+                ),
+                InkWell(
+                  child: Container(
+                    padding: const EdgeInsets.symmetric(vertical: 4),
+                    child: Text(
+                      'View all 50 comments',
+                      // 'View all $commentLen comments',
+                      style: const TextStyle(
+                        fontSize: 16,
+                        color: AppColors.secondary,
+                      ),
+                    ),
+                  ),
+                  // onTap: () => Navigator.of(context).push(
+                  //   MaterialPageRoute(
+                  //     builder: (context) => CommentsScreen(
+                  //       postId: widget.snap['postId'].toString(),
+                  //     ),
+                  //   ),
+                  // ),
+                ),
+                Container(
+                  padding: const EdgeInsets.symmetric(vertical: 4),
+                  child: Text(
+                    '09/02/2011'
+                    // DateFormat.yMMMd()
+                    //     .format(widget.snap['datePublished'].toDate()),
+                    // style: const TextStyle(
+                    //   color: secondaryColor,
+                    // ),
+                  ),
+                ),
+              ],
+            ),
+          ),
+        ],
+      ),
     );
   }
 
