496498d969b81128adc75da07fd952d37e974275
Harsh Patel <hp97808@gmail.com>
2025-08-09 21:19:14 +0530

    fill data

--- lib/widgets/post_card.dart
+++ lib/widgets/post_card.dart
@@ -1,8 +1,11 @@
 import 'package:flutter/material.dart';
 import 'package:instagram/theme/theme.dart';
+import 'package:intl/intl.dart';
 
 class PostCard extends StatelessWidget {
-  const PostCard({super.key});
+  final Map<String, dynamic> snap;
+
+  const PostCard({super.key, required this.snap});
 
   @override
   Widget build(BuildContext context) {
@@ -26,8 +29,7 @@ class PostCard extends StatelessWidget {
                         .titleSmall!
                         .copyWith(fontWeight: FontWeight.w800),
                     child: Text(
-                      '1000 likes',
-                      // '${widget.snap['likes'].length} likes',
+                      '${snap['likes'].length} likes',
                       style: Theme.of(context).textTheme.bodyMedium,
                     )),
                 Container(
@@ -40,14 +42,14 @@ class PostCard extends StatelessWidget {
                       style: const TextStyle(color: AppColors.primary),
                       children: [
                         TextSpan(
-                          text: 'username',
+                          text: snap['username'],
                           // text: widget.snap['username'].toString(),
                           style: const TextStyle(
                             fontWeight: FontWeight.bold,
                           ),
                         ),
                         TextSpan(
-                          text: ' description',
+                          text: ' ${snap['description']}',
                           // text: ' ${widget.snap['description']}',
                         ),
                       ],
@@ -58,8 +60,9 @@ class PostCard extends StatelessWidget {
                   child: Container(
                     padding: const EdgeInsets.symmetric(vertical: 4),
                     child: Text(
-                      'View all 50 comments',
-                      // 'View all $commentLen comments',
+                      // 'View all ${snap['comments'].length} comments',
+
+                      'View all 69 comments',
                       style: const TextStyle(
                         fontSize: 16,
                         color: AppColors.secondary,
@@ -77,12 +80,10 @@ class PostCard extends StatelessWidget {
                 Container(
                   padding: const EdgeInsets.symmetric(vertical: 4),
                   child: Text(
-                    '09/02/2011'
-                    // DateFormat.yMMMd()
-                    //     .format(widget.snap['datePublished'].toDate()),
-                    // style: const TextStyle(
-                    //   color: secondaryColor,
-                    // ),
+                    DateFormat.yMMMd().format(snap['datePublished'].toDate()),
+                    style: const TextStyle(
+                      color: AppColors.secondary,
+                    ),
                   ),
                 ),
               ],
@@ -126,7 +127,7 @@ class PostCard extends StatelessWidget {
       height: MediaQuery.of(context).size.height * 0.35,
       width: double.infinity,
       child: Image.network(
-        'https://plus.unsplash.com/premium_photo-1664474619075-644dd191935f?fm=jpg&q=60&w=3000&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxzZWFyY2h8MXx8aW1hZ2V8ZW58MHx8MHx8fDA%3D',
+        snap['postUrl'],
         fit: BoxFit.cover,
       ),
     );
@@ -143,7 +144,7 @@ class PostCard extends StatelessWidget {
           CircleAvatar(
             radius: 16,
             backgroundImage: NetworkImage(
-              'https://yt3.ggpht.com/yti/ANjgQV8sJ3Ji-ggJxkWTzwW6qwsSQQiARYU9gobaM2O6HUflT6hB=s88-c-k-c0x00ffffff-no-rj',
+              snap['profImage'],
             ),
           ),
           Expanded(
@@ -154,7 +155,7 @@ class PostCard extends StatelessWidget {
                 crossAxisAlignment: CrossAxisAlignment.start,
                 children: [
                   Text(
-                    'username',
+                    snap['username'],
                     style: TextStyle(fontWeight: FontWeight.bold),
                   ),
                 ],
