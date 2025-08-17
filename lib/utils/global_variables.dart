e001288c396c57aa32ba6f9ad68dfe1842a818a2
Harsh Patel <hp97808@gmail.com>
2025-08-17 21:26:24 +0530

    pass uid

--- lib/utils/global_variables.dart
+++ lib/utils/global_variables.dart
@@ -1,3 +1,4 @@
+import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
 import 'package:instagram/screens/add_post_screen.dart';
 import 'package:instagram/screens/feed_screen.dart';
@@ -5,10 +6,10 @@ import 'package:instagram/screens/profile_screen.dart';
 
 import '../screens/search_screen.dart';
 
-const homeScreenItems = [
-  FeedScreen(),
-  SearchScreen(),
-  AddPostScreen(),
-  Text('notif'),
-  ProfileScreen(),
+final homeScreenItems = [
+  const FeedScreen(),
+  const SearchScreen(),
+  const AddPostScreen(),
+  const Text('notif'),
+  ProfileScreen(uid: FirebaseAuth.instance.currentUser!.uid),
 ];
