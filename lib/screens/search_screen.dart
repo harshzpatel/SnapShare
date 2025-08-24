868a3adff9635be264a9267b4b53871f9d80aca2
Harsh Patel <hp97808@gmail.com>
2025-08-24 19:17:47 +0530

    make search round

--- lib/screens/search_screen.dart
+++ lib/screens/search_screen.dart
@@ -27,26 +27,28 @@ class _SearchScreenState extends State<SearchScreen> {
       appBar: AppBar(
         title: TextFormField(
           controller: _searchController,
+          textAlignVertical: TextAlignVertical.center,
           decoration: InputDecoration(
-            // label: Text(hintText),
             labelStyle: TextStyle(color: Color(0xff8f8f8f)),
             hintText: 'Search',
-            // hintStyle: TextStyle(color: Color(0xff8f8f8f)),
             fillColor: Color(0xff121212),
             border: OutlineInputBorder(
+              borderRadius: BorderRadius.circular(30.0),
               borderSide: Divider.createBorderSide(context),
             ),
             focusedBorder: OutlineInputBorder(
+              borderRadius: BorderRadius.circular(30.0),
               borderSide: BorderSide(
-                color: Colors.white.withValues(alpha: .6),
+                color: Colors.white.withAlpha(153),
                 width: 1,
               ),
             ),
             enabledBorder: OutlineInputBorder(
+              borderRadius: BorderRadius.circular(30.0),
               borderSide: BorderSide(color: Color(0xff3b3b3b), width: 1),
             ),
             filled: true,
-            contentPadding: EdgeInsets.all(8),
+            contentPadding: EdgeInsets.symmetric(horizontal: 20.0),
           ),
           onFieldSubmitted: (String _) {
             // setState(() {
@@ -58,82 +60,77 @@ class _SearchScreenState extends State<SearchScreen> {
       body: ValueListenableBuilder(
         valueListenable: _searchController,
         builder: (context, value, child) {
-          return _searchController.text.isNotEmpty
-              ? StreamBuilder(
-                  stream: FirebaseFirestore.instance
-                      .collection('users')
-                      .where(
-                        'username',
-                        isGreaterThanOrEqualTo: _searchController.text,
-                      )
-                      .where(
-                        'username',
-                        isLessThan: '${_searchController.text}\uf8ff',
-                      )
-                      .snapshots(),
-                  builder: (context, snapshot) {
-                    if (snapshot.hasData) {
-                      _searchList = snapshot.data!.docs;
-                    }
+          return _searchController.text.isNotEmpty ? _usersList() : _posts();
+        },
+      ),
+    );
+  }
 
-                    return ListView.builder(
-                      itemCount: _searchList.length,
-                      itemBuilder: (context, index) {
-                        return ListTile(
-                          leading: CircleAvatar(
-                            backgroundImage: AssetImage(
-                              'assets/profile_icon.jpg',
-                            ),
-                            foregroundImage:
-                                _searchList[index]['photoUrl'] != null
-                                ? NetworkImage(_searchList[index]['photoUrl'])
-                                : AssetImage('assets/profile_icon.jpg'),
-                          ),
-                          title: Text(_searchList[index]['username']),
-                          onTap: () {
-                            Navigator.of(context).push(
-                              MaterialPageRoute(
-                                builder: (context) => ProfileScreen(
-                                  uid: _searchList[index]['uid'],
-                                ),
-                              ),
-                            );
-                          },
-                        );
-                      },
-                    );
-                  },
-                )
-              : FutureBuilder(
-                  future: FirebaseFirestore.instance.collection('posts').get(),
-                  builder: (context, snapshot) {
-                    if (snapshot.connectionState == ConnectionState.waiting) {
-                      return Center(child: CircularProgressIndicator());
-                    }
+  StreamBuilder<QuerySnapshot<Map<String, dynamic>>> _usersList() {
+    return StreamBuilder(
+      stream: FirebaseFirestore.instance
+          .collection('users')
+          .where('username', isGreaterThanOrEqualTo: _searchController.text)
+          .where('username', isLessThan: '${_searchController.text}\uf8ff')
+          .snapshots(),
+      builder: (context, snapshot) {
+        if (snapshot.hasData) {
+          _searchList = snapshot.data!.docs;
+        }
 
-                    if (snapshot.hasError) {
-                      return Center(child: Text('Error: ${snapshot.error}'));
-                    }
-
-                    return MasonryGridView.builder(
-                      mainAxisSpacing: 2,
-                      crossAxisSpacing: 2,
-                      gridDelegate:
-                          SliverSimpleGridDelegateWithFixedCrossAxisCount(
-                            crossAxisCount: 2,
-                          ),
-                      itemCount: snapshot.data!.docs.length,
-                      itemBuilder: (context, index) {
-                        return ClipRRect(
-                          borderRadius: BorderRadius.circular(8),
-                          child: Image.network(
-                            '${snapshot.data!.docs[index]['postUrl']}',
-                          ),
-                        );
-                      },
-                    );
-                  },
+        return ListView.builder(
+          itemCount: _searchList.length,
+          itemBuilder: (context, index) {
+            return ListTile(
+              leading: CircleAvatar(
+                backgroundImage: AssetImage('assets/profile_icon.jpg'),
+                foregroundImage: _searchList[index]['photoUrl'] != null
+                    ? NetworkImage(_searchList[index]['photoUrl'])
+                    : AssetImage('assets/profile_icon.jpg'),
+              ),
+              title: Text(_searchList[index]['username']),
+              onTap: () {
+                Navigator.of(context).push(
+                  MaterialPageRoute(
+                    builder: (context) =>
+                        ProfileScreen(uid: _searchList[index]['uid']),
+                  ),
                 );
+              },
+            );
+          },
+        );
+      },
+    );
+  }
+
+  Padding _posts() {
+    return Padding(
+      padding: const EdgeInsets.only(top: 8.0),
+      child: FutureBuilder(
+        future: FirebaseFirestore.instance.collection('posts').get(),
+        builder: (context, snapshot) {
+          if (snapshot.connectionState == ConnectionState.waiting) {
+            return Center(child: CircularProgressIndicator());
+          }
+
+          if (snapshot.hasError) {
+            return Center(child: Text('Error: ${snapshot.error}'));
+          }
+
+          return MasonryGridView.builder(
+            mainAxisSpacing: 2,
+            crossAxisSpacing: 2,
+            gridDelegate: SliverSimpleGridDelegateWithFixedCrossAxisCount(
+              crossAxisCount: 2,
+            ),
+            itemCount: snapshot.data!.docs.length,
+            itemBuilder: (context, index) {
+              return Image.network(
+                '${snapshot.data!.docs[index]['postUrl']}',
+              );
+            },
+          );
         },
       ),
     );
