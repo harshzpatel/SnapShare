0ffa18163c1fb23ceb56dd5913c5b64225808d1b
Harsh Patel <hp97808@gmail.com>
2025-08-28 22:50:14 +0530

    add send message, get message

--- lib/services/chat_service.dart
+++ lib/services/chat_service.dart
@@ -1,7 +1,10 @@
 import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:firebase_auth/firebase_auth.dart';
+import 'package:snapshare/models/message.dart';
 
 class ChatService {
   final _firestore = FirebaseFirestore.instance;
+  final _auth = FirebaseAuth.instance;
 
   Stream<List<Map<String, dynamic>>> getUsersStream() {
     return _firestore.collection('users').snapshots().map((snapshot) {
@@ -9,5 +12,43 @@ class ChatService {
         return doc.data();
       }).toList();
     });
+  }
+
+  Future<void> sendMessage(String receiverId, String message) async {
+    final String senderId = _auth.currentUser!.uid;
+    final String senderEmail = _auth.currentUser!.email!;
+    final Timestamp timestamp = Timestamp.now();
+
+    Message newMessage = Message(
+      senderId: senderId,
+      receiverId: receiverId,
+      message: message,
+      timestamp: timestamp,
+    );
+
+    List<String> userIds = [senderId, receiverId];
+    userIds.sort();
+    String chatId = userIds.join('_');
+
+    await _firestore
+        .collection('chats')
+        .doc(chatId)
+        .collection('messages')
+        .add(newMessage.toMap());
+  }
+
+  // get messages stream between two users
+  Stream<QuerySnapshot> getMessagesStream(String otherUserId) {
+    final String currentUserId = _auth.currentUser!.uid;
+    List<String> userIds = [currentUserId, otherUserId];
+    userIds.sort();
+    String chatId = userIds.join('_');
+
+    return _firestore
+        .collection('chats')
+        .doc(chatId)
+        .collection('messages')
+        .orderBy('timestamp', descending: false)
+        .snapshots();
   }
 }
